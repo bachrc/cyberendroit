520212568cfa07a8b80910609b6a486af2b90a97
bachrc <bachrc@dessert.coffee>
2022-10-05 21:23:38 +0200

    La Svelte Typography fonctionne

--- src/routes/articles.json/+server.ts
+++
@@ -1,9 +0,0 @@
-import {loadArticles} from "$lib/articles";
-
-export const prerender = true;
-
-export async function GET() {
-    const posts = await loadArticles()
-
-    return new Response(JSON.stringify(posts, null, 2))
-}
--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-    export let data;
-    const articles = data.articles;
+    import type {ArticleMetadata, ArticlesFromServer} from "../../lib/models";
+
+    export let data: ArticlesFromServer;
+    const articles: ArticleMetadata[] = data.articles;
 </script>
 
 <div class="flex flex-col gap-8">
     {#each articles as article}
         <a href="{article.url}">
             <span class="text-2xl">{article.title}</span>
-            <p class="text-lg">Description</p>
+            <p class="text-lg">{article.description}</p>
         </a>
     {/each}
 </div>
